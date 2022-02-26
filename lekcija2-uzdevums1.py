8d61ba249db801ac3bb5cd520b4eb245b8e69b72
dainisivbulis <dainis.ivbulis@gmail.com>
2022-02-26 14:37:09 +0200

    Pirmais uzdeums

--- lekcija2-uzdevums1.py
+++ lekcija2-uzdevums1.py
@@ -1,10 +1,8 @@
-sk1 = 13
-sk2 = 7
-sk3 = 7
+#Uzrakstiet programmu, kas prasa lietotājam ievadīt savu vārdu, ja tas ievada vārdu, Bond, tad izdrukājiet uz ekrāna "Esi sveicināts 007",
+#ja cits tad "Esi sveicināts, VARDS", kur VARDS ir lietotāja ievadītais vārds.
 
-print("sk1 == sk2:", sk1 == sk2)
-print("sk1 != sk2:", sk1 != sk2)
-print("sk1 < sk2:", sk1 < sk2)
-print("sk1 > sk2:", sk1 > sk2)
-print("sk1 <= sk2:", sk1 <= sk2)
-print("sk1 >= sk2:", sk1 >= sk2)
+vards = input ("Ievadi savu vārdu: ")
+if vards == "Bond":
+    print("Esi sveicināts 007")
+else:
+    print("Esi sveicināts,", vards)
