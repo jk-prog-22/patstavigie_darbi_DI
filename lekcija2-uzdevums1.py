4ae36c760fa8fc8b9290ae37743a3a643c6d8eeb
dainisivbulis <dainis.ivbulis@gmail.com>
2022-02-26 14:08:53 +0200

    Parbaude

---
+++ lekcija2-uzdevums1.py
@@ -0,0 +1,10 @@
+sk1 = 13
+sk2 = 7
+sk3 = 7
+
+print("sk1 == sk2:", sk1 == sk2)
+print("sk1 != sk2:", sk1 != sk2)
+print("sk1 < sk2:", sk1 < sk2)
+print("sk1 > sk2:", sk1 > sk2)
+print("sk1 <= sk2:", sk1 <= sk2)
+print("sk1 >= sk2:", sk1 >= sk2)
